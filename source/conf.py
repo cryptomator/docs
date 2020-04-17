6249010fd97e5d3b11921adec65031b17b7464c1
Armin Schrenk <armin.schrenk@zoho.eu>
2020-04-17 16:54:30 +0200

    adding our customized sphinx read the docs theme

--- source/conf.py
+++ source/conf.py
@@ -38,11 +38,9 @@ release = '1.5.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-import sphinx_rtd_theme
-
+#
 extensions = [
-    'sphinx.ext.todo',
-    'sphinx_rtd_theme'
+    'sphinx.ext.todo'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -78,7 +76,8 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'customized_sphinx_rtd_theme'
+html_theme_path = ['_themes', ]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -92,19 +91,17 @@ html_theme_options = {
     'includehidden': True,
     'titles_only': False,
     'logo_only' : True,
-    'style_nav_header_background' : '#49B04A'
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-
-html_css_files = [
-    'css/custom.css',
-]
-
-html_style = 'css/custom.css'
+#
+#html_static_path = ['_static']
+#html_css_files = [
+#    'css/custom.css',
+#]
+#html_style = 'css/custom.css'
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
