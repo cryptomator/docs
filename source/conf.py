5aca6af6a53a10442c3f231a6ce0837cb411ddb4
infeo <armin.schrenk@zoho.eu>
2020-04-21 16:53:37 +0200

    Moving submodule to avoid usage of symlinks

--- source/conf.py
+++ source/conf.py
@@ -65,7 +65,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_themes/*']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = None
@@ -76,8 +76,8 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'customized_sphinx_rtd_theme'
-html_theme_path = ['_themes', ]
+html_theme = 'sphinx_rtd_theme'
+html_theme_path = ['_themes/cryptomator_theme', ]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
