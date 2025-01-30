40b9810549857d72a3267e630c0b7606c2261fd2
Julian Raufelder <Julian@Raufelder.com>
2025-01-30 14:35:53 +0100

    Set baseUrl to see if this removes the /index from the root

--- source/conf.py
+++ source/conf.py
@@ -107,6 +107,7 @@ html_theme_options = {
 html_static_path = ['_static']
 html_css_files = []
 html_style = 'css/overrides.css'
+html_baseurl = 'https://docs2.cryptomator.org/'
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
