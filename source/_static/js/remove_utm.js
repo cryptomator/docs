74bb5dcb9c99c520178f6cf73297d23617b9c433
Julian Raufelder <Julian@Raufelder.com>
2025-03-13 11:27:42 +0100

    Remove utm params from address bar at load

---
+++ source/_static/js/remove_utm.js
@@ -0,0 +1,25 @@
+    function removeUTMParams() {
+      const url = new URL(window.location.href);
+      const params = url.searchParams;
+      const utmParams = ['utm_source', 'utm_medium', 'utm_campaign', 'utm_term', 'utm_content'];
+      let modified = false;
+      utmParams.forEach(param => {
+        if (params.has(param)) {
+          params.delete(param);
+          modified = true;
+        }
+      });
+      if (modified) {
+        const newQuery = params.toString() ? '?' + params.toString() : '';
+        const newUrl = url.pathname + newQuery + url.hash;
+        window.history.replaceState({}, document.title, newUrl);
+      }
+    }
+    if ('requestIdleCallback' in window) {
+      requestIdleCallback(removeUTMParams);
+    } else {
+      // Fallback if not supported
+      window.addEventListener('load', () => {
+        removeUTMParams();
+      });
+    }
